2257a7ffb858ee0d8e18d10b9cd20cb9eba014fd
Dmitrii <chertov.dmitrii@yandex.ru>
2022-06-12 07:38:56 +0300

    второй комит.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.time.Year;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!!!K");
+
+    public static int checkYear(int[] years) {
+        int year = 0;
+        for (int i = 0; i < years.length; i++) {
+            year = years[i] % 4;
+            if (year == 0 || years[i] % 400 == 0 || years[i] % 100 == 0) {
+                System.out.println(years[i] + " год является високосным.");
+            } else if (year != 0) {
+                System.out.println(years[i] + " год не является високосным.");
+            }
+            }
+            return year;
+        }
+
+        public static void main (String[]args){
+            int[] years = {2000, 2002, 2004, 2006,};
+            checkYear(years);
+        }
     }
-}
