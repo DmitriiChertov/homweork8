107c8b479745b7617cae6503a6c139416e43f3c0
Dmitrii <chertov.dmitrii@yandex.ru>
2022-06-12 10:17:56 +0300

    третий комит.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 import java.time.Year;
 
 public class Main {
+    public static void printSeparator() {
+        System.out.println();
+        System.out.println("------------------");
+        System.out.println();
+    }
 
     public static int checkYear(int[] years) {
         int year = 0;
@@ -11,12 +16,33 @@ public class Main {
             } else if (year != 0) {
                 System.out.println(years[i] + " год не является високосным.");
             }
-            }
-            return year;
         }
-
-        public static void main (String[]args){
-            int[] years = {2000, 2002, 2004, 2006,};
-            checkYear(years);
-        }
+        return year;
     }
+    public static int checkApp(int os, int year) {
+        if ((os == 1) && (year >= 2015)) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if ((os == 1) && (year < 2015)){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if ((os == 0) && (year >= 2015)) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        return os;
+    }
+
+    public static void main(String[] args) {
+        int[] years = {2000, 2002, 2004, 2006,};
+        printSeparator();
+        checkYear(years);
+        printSeparator();
+
+        int os = 0;
+        int year = 2016;
+        checkApp(os, year);
+        printSeparator();
+
+
+    }
+}
